99d1da714028b67f27ce2e5e7110f440636bf093
Pavelabramovich <apn20040510@gmail.com>
2023-12-13 09:42:35 +0300

    add grid logic

--- Shared/Models/BattleSquare.cs
+++ Shared/Models/BattleSquare.cs
@@ -1,13 +1,39 @@
 ﻿
+using System.Drawing;
+
 namespace BattleShip.Shared.Models;
 
 
-// Implementation may change
-public readonly struct BattleSquare
+/// <include file='Documentation/BattleSquare.xml' path='doc/class[@name="BattleSquare"]/description' />
+public class BattleSquare
 {
-    [Flags]
-    enum Flag : byte
-    {
+    public ShotStatus ShotStatus { get; set; }
+    public OrientedShipPart? OrientedShipPart { get; private set; }
 
+
+    public BattleSquare(ShotStatus shotStatus, OrientedShipPart? orientedShipPart)
+    {
+        ShotStatus = shotStatus;
+        OrientedShipPart = orientedShipPart;
+    }
+
+    public void Deconstruct(out ShotStatus shotStatus, out OrientedShipPart? orientedShipPart)
+    {
+        shotStatus = ShotStatus;
+        orientedShipPart = OrientedShipPart;
+    }
+
+    /// <include file='Documentation/BattleSquare.xml' path='doc/class[@name="BattleSquare"]/method[@name="ToString"]' />
+    public override string ToString()
+    {
+        return this switch
+        {
+            (ShotStatus.Intact, orientedShipPart: null) => " ",
+            (ShotStatus.Intact, orientedShipPart: not null) => "█",
+            (ShotStatus.Shotted, orientedShipPart: null) => "·",
+            (ShotStatus.Shotted, orientedShipPart: not null) => "X",
+            (ShotStatus.Destroyed, _) => "☼",
+            _ => throw new NotImplementedException("Uncnown pattern."),
+        };
     }
 }
